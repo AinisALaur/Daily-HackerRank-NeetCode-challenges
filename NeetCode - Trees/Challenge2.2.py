9b88012d7185b57e6bcc214ba91daccafb99aecf
AinisALaur <ainislaurinavicius@gmail.com>
2026-06-10 13:31:25 +0300

    Challenge 2 iteratitive solution

---
+++ NeetCode - Trees/Challenge2.2.py
@@ -0,0 +1,34 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def preorderTraversal(self, root: TreeNode) -> list[int]:
+        res = []
+        curr = root
+        stack = []
+
+        while curr or stack:
+            while curr:
+                stack.append(curr)
+                res.append(curr.val)
+                curr = curr.left
+            
+            curr = stack.pop()
+            curr = curr.right
+
+        return res
+
+node6 = TreeNode(6)
+node7 = TreeNode(7)
+node4 = TreeNode(4)
+node5 = TreeNode(5)
+node2 = TreeNode(2, node4, node5)
+node3 = TreeNode(3, node6, node7)
+node1 = TreeNode(1, node2, node3)
+
+sol = Solution()
+print(sol.preorderTraversal(node1))
