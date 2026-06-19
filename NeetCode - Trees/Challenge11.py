2ba6e654e0e9627ecea6655a8a1aa8ee69b33c32
AinisALaur <ainislaurinavicius@gmail.com>
2026-06-19 11:04:40 +0300

    Challenge 11 code

---
+++ NeetCode - Trees/Challenge11.py
@@ -0,0 +1,17 @@
+class Solution:
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if not root:
+            return TreeNode(val)
+
+        curr = root
+        while True:
+            if val > curr.val:
+                if not curr.right:
+                    curr.right = TreeNode(val)
+                    return root
+                curr = curr.right
+            else:
+                if not curr.left:
+                    curr.left = TreeNode(val)
+                    return root
+                curr = curr.left
